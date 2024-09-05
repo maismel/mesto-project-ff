56daf8af6456f88c8551ffb45fee1e99a56a4839
Katherine Vasilevskaya <katevasilevskaya0@gmail.com>
2024-09-05 21:39:05 +0300

    для второго ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,37 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
 
 const cardList = document.querySelector('.places__list');
 
-
 // @todo: Функция создания карточки
 
-function addCard(card) {
-    const cardTemplate = document.querySelector('#card-template').content;
+function createCard (card, deleteCard) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('.card__image').src = card.link;
-    cardElement.querySelector('.card__image').alt = card.name;
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.src = card.link;
+    cardImage.alt = card.name;
     cardElement.querySelector('.card__title').textContent = card.name;
 
     const deleteButton = cardElement.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', function(evt) {
-        const eventTarget = evt.target;
-        const card = eventTarget.closest('.card');
-        card.remove();
-    })
+    deleteButton.addEventListener('click', deleteCard);
+    
     return cardElement;
 }
 
 // @todo: Функция удаления карточки
 
+function deleteCard (evt) {
+    const eventTarget = evt.target;
+    const card = eventTarget.closest('.card');
+    card.remove();
+}
+
 // @todo: Вывести карточки на страницу
 
-initialCards.forEach (function(card) {
-    const newCard = addCard(card);
+initialCards.forEach (function (card) {
+    const newCard = createCard(card, deleteCard);
     cardList.append(newCard);
 })
